4e3906b9c8222caf4d7086d728c45262585aad5a
sbhatt <sh224186@dal.ca>
2022-03-22 19:54:15 -0300

    data

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  basePath: "data/",
-  profilePath: "profile_info.json",
+  basePath: "https://extraordinary-cannoli-dc10ba.netlify.app/.netlify/functions/",
+  profilePath: "profile-data",
   i18nKeyStorePath: "i18n_keystore.json"
 };
